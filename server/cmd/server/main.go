1edd8ae9f5d240dc604f722d39ce7980e81dce92
Michal Kukla <michal.kukla@tul.cz>
2023-12-11 09:28:53 +0100

    server: main.go - gofmt

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -1,13 +1,12 @@
 package main
 
 import (
+	"crypto/md5"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"net/http"
 	"net/url"
-	"crypto/md5"
-
 
 	"github.com/Solamil/bp23/server"
 )
@@ -17,9 +16,10 @@ type geoUrlParams struct {
 }
 
 type ticket struct {
-	Obu server.OnBoardUnit `json:"obu"`
-	CheckPoints server.Polygon `json:"polygon"`
+	Obu         server.OnBoardUnit `json:"obu"`
+	CheckPoints server.Polygon     `json:"polygon"`
 }
+
 const PORT = 8905
 
 var dbType string = "Blockchain"
@@ -53,14 +53,14 @@ func index_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func ticket_handler(w http.ResponseWriter, r *http.Request) {
-	t := &ticket{}	
+	t := &ticket{}
 	if err := json.NewDecoder(r.Body).Decode(t); err != nil {
 		fmt.Println(err.Error())
 	}
 	o := t.Obu
-	obu := server.GetObu(o.ID, o.SPZ, o.Country, dbType)	
+	obu := server.GetObu(o.ID, o.SPZ, o.Country, dbType)
 	if obu == nil {
-		w.Write([]byte("Not found"))	
+		w.Write([]byte("Not found"))
 		// handle unexpexted OBU
 		return
 	}
@@ -72,18 +72,18 @@ func ticket_handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func obu_handler(w http.ResponseWriter, r *http.Request) {
-	var o server.OnBoardUnit	
+	var o server.OnBoardUnit
 	if err := json.NewDecoder(r.Body).Decode(&o); err != nil {
 		fmt.Println(err.Error())
 	}
-	obu := server.GetObu(o.ID, o.SPZ, o.Country, dbType)	
+	obu := server.GetObu(o.ID, o.SPZ, o.Country, dbType)
 	if obu == nil {
 		w.Write([]byte("error: OBU not found"))
 		return
 	}
 	if obu.Emission != o.Emission || obu.Weight != o.Weight || obu.Axles != o.Axles {
-		server.UpdateObu(o.ID, o.SPZ, o.Country, o.Emission, 
-						fmt.Sprintf("%d", o.Weight), fmt.Sprintf("%d", o.Axles), dbType)
+		server.UpdateObu(o.ID, o.SPZ, o.Country, o.Emission,
+			fmt.Sprintf("%d", o.Weight), fmt.Sprintf("%d", o.Axles), dbType)
 		w.Write([]byte("Modified parameters in OBU"))
 	}
 	byteJson, _ := json.Marshal(obu)
@@ -102,10 +102,10 @@ func geo_handler(w http.ResponseWriter, r *http.Request) {
 		var param *geoUrlParams
 		json.Unmarshal(js, &param)
 		if param.Version != nil {
-			opt = "v" 
+			opt = "v"
 		}
 
-	} 
+	}
 	if opt == "v" {
 		var versions []server.WptRecords
 		server.LoadModel()
@@ -114,12 +114,12 @@ func geo_handler(w http.ResponseWriter, r *http.Request) {
 			section.Version = v.Version
 			section.Name = v.Name
 			section.Checksum = hash([]byte(fmt.Sprintf("%+v", v)))
-			versions = append(versions, section)	
+			versions = append(versions, section)
 		}
-		result, _ = json.Marshal(versions)		
+		result, _ = json.Marshal(versions)
 	} else {
 		server.LoadModel()
-		result, _ = json.Marshal(server.Model)	
+		result, _ = json.Marshal(server.Model)
 	}
 
 	w.Write([]byte(result))
@@ -127,42 +127,42 @@ func geo_handler(w http.ResponseWriter, r *http.Request) {
 
 func processTicket(t ticket) float64 {
 	var distance float64 = 0.0
-	var sazba float64 = 0.0 
+	var sazba float64 = 0.0
 	var roadname string = ""
-	var timestamp string = "" 
+	var timestamp string = ""
 	model := server.Model
-	
+
 	obu := t.Obu
 	p := t.CheckPoints
 	var i int = 0
-	for ; i < len(p.I) - 1; i++ {
+	for ; i < len(p.I)-1; i++ {
 		if p.I[i] == p.I[i+1] && server.IsDay(p.Time[i]) == server.IsDay(p.Time[i+1]) {
-			//still the same paid road section, and still the same day or night 
+			//still the same paid road section, and still the same day or night
 			lat1 := model[p.I[i]].LatRad[p.J[i]]
 			lon1 := model[p.I[i]].LonRad[p.J[i]]
 			lat2 := model[p.I[i+1]].LatRad[p.J[i+1]]
 			lon2 := model[p.I[i+1]].LonRad[p.J[i+1]]
 			distance += server.Haversine(lat1, lon1, lat2, lon2)
-		} else if p.I[i] != p.I[i+1] || server.IsDay(p.Time[i]) != server.IsDay(p.Time[i+1])  {
+		} else if p.I[i] != p.I[i+1] || server.IsDay(p.Time[i]) != server.IsDay(p.Time[i+1]) {
 			//end of the same paid road section, or changed from daytime to nightime and vice versa
 			//For each road section there are different charge and for daytime and nightime
 			roadname = model[p.I[i]].Name
-			timestamp = p.Time[i] 		
-			sazba += server.ExecSazba(distance, timestamp, obu.Weight, 
+			timestamp = p.Time[i]
+			sazba += server.ExecSazba(distance, timestamp, obu.Weight,
 				obu.Axles, obu.Category, obu.Emission, roadname)
 
 			distance = 0.0
 		}
 
 	}
-	
+
 	roadname = model[p.I[i]].Name
-	timestamp = p.Time[i] 		
-	sazba += server.ExecSazba(distance, timestamp, obu.Weight, 
+	timestamp = p.Time[i]
+	sazba += server.ExecSazba(distance, timestamp, obu.Weight,
 		obu.Axles, obu.Category, obu.Emission, roadname)
 
-	return sazba 
-} 
+	return sazba
+}
 
 func hash(data []byte) string {
 	return fmt.Sprintf("%x", md5.Sum(data))
